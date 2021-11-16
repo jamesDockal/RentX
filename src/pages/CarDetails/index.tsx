572df50f74ac6f5a3795cfa72d5c6bd1e15efa21
James <jamesistranho@gmail.com>
2021-11-16 18:53:09 -0300

    create CarDetails Page

---
+++ src/pages/CarDetails/index.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Accessory } from "../../components/Accessory";
+
+import { BackButton } from "../../components/BackButton";
+import { ImagesSlider } from "../../components/ImagesSlider";
+import {
+  Container,
+  Header,
+  ImageContainer,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  AcessoriesWrapper,
+  Footer,
+} from "./styles";
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+import { Button } from "../../components/Button";
+
+type Props = {};
+
+export const CarDetails: React.FC<Props> = ({}) => {
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+
+      <ImageContainer>
+        <ImagesSlider
+          imageUrl={[
+            "https://img2.gratispng.com/20180816/paa/kisspng-ferrari-s-p-a-car-ferrari-488-enzo-ferrari-ferrari-png-free-png-images-toppng-5b75fbb0461eb8.4406679715344588002872.jpg",
+          ]}
+        />
+      </ImageContainer>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Hiracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <AcessoriesWrapper>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 Pessoas" icon={PeopleSvg} />
+        </AcessoriesWrapper>
+        <About>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          adipisci accusamus ratione cum magnam cumque quasi quos dolor eveniet
+          animi maxime, sunt itaque et nostrum. Ut illo dolorem accusamus?
+          Recusandae!
+        </About>
+      </Content>
+
+      <Footer>
+        <Button name="Confirmar" />
+      </Footer>
+    </Container>
+  );
+};
---
+++ src/pages/CarDetails/styles.ts
@@ -0,0 +1,93 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  position: absolute;
+  margin-top: 18px;
+  margin-left: 24px;
+`;
+
+export const ImageContainer = styled.View`
+  margin-top: 32px;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: "center",
+  },
+})``;
+
+export const Details = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(25)}px;
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+`;
+
+export const Price = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.main};
+  font-size: ${RFValue(25)}px;
+`;
+
+export const AcessoriesWrapper = styled.View`
+  width: 100%;
+  margin-top: 16px;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const About = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(15)}px;
+  line-height: ${RFValue(25)}px;
+
+  text-align: justify;
+  margin-top: 32px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+
+  padding: 24px 24px;
+`;
